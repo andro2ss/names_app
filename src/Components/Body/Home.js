2beb0f833d4292702f299bf3eb798b666ea0f9c8
andro2ss <a.brzeski@hotmail.com>
2022-01-20 04:11:46 -0800

    raw version Home page

--- src/Components/Body/Home.js
+++ src/Components/Body/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function Home(props) {
     return (
-        <div>banany jem gdy nie aptrza</div>
+        <Container fixed>
+            <Box sx={{bgcolor: '#cfe8fc', height: '100%'}}>
+                <h1>Names Info</h1>
+                <hr/>
+                <h3> Miło że jesteś z nami !!!</h3>
+                <p>Dzięki nam dowiesz się</p>
+                <ul>
+                    <li>co nieco o swoim imieniu,</li>
+                    <li>Jakie są najpopularniejsze imiona w Polsce,</li>
+                    <li> a także wygenerujesz imię dla swojej pociechy.</li>
+                </ul>
+                <p>Zapraszam do Zabawy</p>
+            </Box>
+        </Container>
     );
 }
 
