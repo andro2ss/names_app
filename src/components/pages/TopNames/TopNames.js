4a30943011f7f00bdf18f8f8aaef981ff2429c88
andro2ss <a.brzeski@hotmail.com>
2022-01-25 04:47:34 -0800

    styled page container in app.js , fixed double container style in home page, aplha version style in topNames done

--- src/components/pages/TopNames/TopNames.js
+++ src/components/pages/TopNames/TopNames.js
@@ -1,28 +1,59 @@
 import React from "react";
 import BasicTable from "../../common/items/Table";
+import "./TopNames.scss";
 
 function TopNames({ status, arrays, counters }) {
+  const handleClick = (e) => {
+    if (e.target.innerText === "Męskie") {
+      document.querySelector(".box--menu").firstChild.classList.add("active");
+      document.querySelector(".box--menu").lastChild.classList.remove("active");
+      document.querySelector(".tables__box").firstChild.classList.add("active");
+      document
+        .querySelector(".tables__box")
+        .lastChild.classList.remove("active");
+    } else {
+      document
+        .querySelector(".box--menu")
+        .firstChild.classList.remove("active");
+      document.querySelector(".box--menu").lastChild.classList.add("active");
+      document
+        .querySelector(".tables__box")
+        .firstChild.classList.remove("active");
+      document.querySelector(".tables__box").lastChild.classList.add("active");
+    }
+  };
+
   return (
     <>
       <div className="topNames">
-        <h2>Najpopularniejsze imiona</h2>
-        <div>
-          <p>Panowie</p>
-          <BasicTable
-            status={status}
-            arrays={arrays}
-            counters={counters}
-            selectedArr={0}
-            rangeFilter={[0, 100]}
-          />
-          <p>Panie</p>
-          <BasicTable
-            status={status}
-            arrays={arrays}
-            counters={counters}
-            selectedArr={1}
-            rangeFilter={[0, 100]}
-          />
+        <h2 className="title">Najpopularniejsze imiona</h2>
+        <div className="box--menu">
+          <button className="button--gender active" onClick={handleClick}>
+            Męskie
+          </button>
+          <button className="button--gender" onClick={handleClick}>
+            Żeńskie
+          </button>
+        </div>
+        <div className="tables__box">
+          <div className="table active">
+            <BasicTable
+              status={status}
+              arrays={arrays}
+              counters={counters}
+              selectedArr={0}
+              rangeFilter={[0, 100]}
+            />
+          </div>
+          <div className="table">
+            <BasicTable
+              status={status}
+              arrays={arrays}
+              counters={counters}
+              selectedArr={1}
+              rangeFilter={[0, 100]}
+            />
+          </div>
         </div>
       </div>
     </>
